a3d69fb9faa5b11f569ad7ebcafcd5c244d05e1b
cacti23 <patel.aditya2014@gmail.com>
2023-04-25 13:52:02 +0530

    feat(comments): emitting creation event for comments

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -23,6 +24,15 @@ app.post("/posts/:id/comments", (req, res) => {
   comments.push({ id: commentId, content });
 
   commentsByPostId[req.params.id] = comments;
+
+  axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
 
   res.status(201).send(comments);
 });
